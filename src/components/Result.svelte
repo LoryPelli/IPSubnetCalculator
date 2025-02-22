b29c9284fb57ddb0a4b493c81cfb8cc71cb24e1a
lorypelli <lorypelli@proton.me>
2025-02-22 11:50:26 +0100

    show current IP and number of subnets

---
+++ src/components/Result.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    interface Props {
+        show: boolean;
+        ip: string;
+        subnets: number;
+    }
+    const { show, ip, subnets }: Props = $props();
+</script>
+
+{#if show}
+    <span>IP: {ip}</span>
+    <span>Number of subnets: {subnets}</span>
+{/if}
