fc22fc89a5901897851ff32716d73d8a58b5adb4
lorypelli <lorypelli@proton.me>
2025-03-20 20:52:03 +0100

    fix network and broadcast IPs

--- src/utils/find.ts
+++ src/utils/find.ts
@@ -6,8 +6,12 @@ export function findSubnet(
     subnetBits: number,
 ) {
     const host = ip.slice(networkBits);
-    const hostBits = 32 - networkBits;
-    if (host == '0'.repeat(hostBits) || host == '1'.repeat(hostBits)) {
+    const withSubnet = host.slice(subnetBits);
+    const hostBits = 32 - networkBits - subnetBits;
+    if (
+        withSubnet == '0'.repeat(hostBits) ||
+        withSubnet == '1'.repeat(hostBits)
+    ) {
         return -1;
     }
     return parseInt(host.slice(0, subnetBits), 2) || 0;
@@ -15,8 +19,12 @@ export function findSubnet(
 
 export function findHost(ip: string, networkBits: number, subnetBits: number) {
     const host = ip.slice(networkBits);
-    const hostBits = 32 - networkBits;
-    if (host == '0'.repeat(hostBits) || host == '1'.repeat(hostBits)) {
+    const withSubnet = host.slice(subnetBits);
+    const hostBits = 32 - networkBits - subnetBits;
+    if (
+        withSubnet == '0'.repeat(hostBits) ||
+        withSubnet == '1'.repeat(hostBits)
+    ) {
         return -1;
     }
     return parseInt(host.slice(subnetBits), 2) || 1;
@@ -30,9 +38,8 @@ export function findIP(
     subnetBits: number,
 ) {
     const binarySubnet = subnet.toString(2).padStart(subnetBits, '0');
-    const binaryHost = host
-        .toString(2)
-        .padStart(32 - networkBits - subnetBits, '0');
+    const hostBits = 32 - networkBits - subnetBits;
+    const binaryHost = host.toString(2).padStart(hostBits, '0');
     const network = startIP.slice(0, networkBits);
     const ip = network + binarySubnet + binaryHost;
     return decimal(ip);
